7d58627bd4563bef9175160f7f4295d27695f2d4
Anton Hunter <sdia7sdia@gmail.com>
2024-03-14 13:41:49 +1030

    some improvements to request handler and cors policy

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 const morgan = require("morgan");
 
 const PORT = 3490;
