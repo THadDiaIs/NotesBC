c01e3371403cff85e8b20881b3dce3a91a6c16fa
Anton Hunter <sdia7sdia@gmail.com>
2024-03-26 22:55:48 +1030

    connected to a mongo databse to store and retirve data in a separated file called mongo.js, and its avaliable to use from cli

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+
+if (process.argv.length < 3){
+    console.log("password not passed as argument");
+    process.exit(1);
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://sdia7sdia:${encodeURIComponent(password)}@dnn0.k280uaz.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Dnn0`;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({ name: String, number: String });
+// const Person = new mongoose.model('Person', personSchema);
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length < 4){
+    Person.find({}).then(result => {
+        console.log('Phonebook:')
+        result.forEach(person => {
+            console.log(`${person.name} : ${person.number}`);
+        });
+        mongoose.connection.close();
+    });
+} else {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    });
+
+    person.save().then(result => {
+        console.log(`Added ${person.name} with number ${person.number} to phonebook!`);
+        mongoose.connection.close();
+    });
+
+}
+
+
+
