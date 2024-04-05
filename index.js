da1f840bb34466f14b9da4f977ef6f6b403c2f55
Anton Hunter <sdia7sdia@gmail.com>
2024-04-05 12:34:39 +1030

    added linter adn corrected some errors, request unused variables warnings not fixed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const Person = require("./models/person");
 const app = express();
 
-morgan.token('req-content', req => req.body ? JSON.stringify(req.body) : " ");
+morgan.token("req-content", req => req.body ? JSON.stringify(req.body) : " ");
 
 app.use(express.static("dist"));
 app.use(express.json());
@@ -14,72 +14,72 @@ app.use(morgan(":method :url :status :res[content-length] - :response-time ms :r
 
 
 app.get("/info", (req, resp) => {
-    Person.find({}).then(result => {
+  Person.find({}).then(result => {
     resp.send(`<h1 style='
-    background-color:black;
-    color:white;
-    text-align:center;
-    padding:1em'>
-        This is the phonebook API</h1>
-    <p>There are actually, ${result.length} persons in the phonebook.</p>
-    <footer style='
-    width:100%;
-    text-align:center;
-    background-color:gray;
-    color:white'>${new Date()}</footer>`)
-    })
+  background-color:black;
+  color:white;
+  text-align:center;
+  padding:1em'>
+    This is the phonebook API</h1>
+  <p>There are actually, ${result.length} persons in the phonebook.</p>
+  <footer style='
+  width:100%;
+  text-align:center;
+  background-color:gray;
+  color:white'>${new Date()}</footer>`);
+  });
 });
 
 app.get("/api/persons", (req, resp) => {
-    Person.find({}).then(result => resp.json(result));
-})
+  Person.find({}).then(result => resp.json(result));
+});
 
 app.get("/api/persons/:id", (req, resp, next) => {
-    Person.findById(req.params.id)
-        .then(person => resp.json(person))
-        .catch(error => next(error));
+  Person.findById(req.params.id)
+    .then(person => resp.json(person))
+    .catch(error => next(error));
 });
 
 app.delete("/api/persons/:id", (req, resp, next) => {
-    Person.findByIdAndDelete(req.params.id)
-        .then(result => resp.status(204).end())
-        .catch(error => next(error));
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => resp.status(204).end())
+    .catch(error => next(error));
 });
 
 app.post("/api/persons", (req, resp, next) => {
-    const data = req.body;
-    const person =  new Person({
-        name : data.name,
-        number: data.number
-    });
+  const data = req.body;
+  const person =  new Person({
+    name : data.name,
+    number: data.number
+  });
 
-    person.save()
-        .then(svdPrsn => resp.json(svdPrsn))
-        .catch(error => next(error));
+  person.save()
+    .then(svdPrsn => resp.json(svdPrsn))
+    .catch(error => next(error));
 });
 
 app.put("/api/persons/:id", (req, resp, next) => {
-    const {name, number} = req.body;
-    Person.findByIdAndUpdate(req.params.id,
-                             {name, number},
-                             { new : true, runValidators: true, context: 'query'})
+  const { name, number } = req.body;
+  Person.findByIdAndUpdate(req.params.id,
+    { name, number },
+    { new : true, runValidators: true, context: "query" })
     .then(updatedPerson => resp.json(updatedPerson))
     .catch(error => next(error));
 });
 
 const unknownEndpoint = (req, resp) => {
-    resp.status(404).send({ error: 'unknown endpoint' });
-}
+  resp.status(404).send({ error: "unknown endpoint" });
+};
 app.use(unknownEndpoint);
 
-const errorHandler = (error, req, resp, next) =>{
-    if (error.name === 'CastError'){
-        return resp.status(400).send({ error: 'mlformatted id'});
-    } else if (error.name === 'ValidationError'){
-        return resp.status(400).send({error: error.message})
-    }
-    next(error);
-}
+const errorHandler = (error, req, resp, next) => {
+  if (error.name === "CastError"){
+    return resp.status(400).send({ error: "mlformatted id" });
+  } else if (error.name === "ValidationError"){
+    return resp.status(400).send({ error: error.message });
+  }
+  next(error);
+};
 app.use(errorHandler);
 
 const PORT = process.env.PORT;
--- models/person.js
+++ models/person.js
@@ -1,35 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const url = process.env.MONGODB_URI;
 
-mongoose.set('strictQuery', false);
+mongoose.set( "strictQuery", false);
 mongoose.connect(url)
-    .then(result => console.log('DB connection sucessfull!'))
-    .catch(error => console.log('Error connecting to MongoDB: ',error.message));
+  .then(result => console.log("DB connection sucessfull!"))
+  .catch(error => console.log("Error connecting to MongoDB:",error.message));
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function(v){
+        return /\d{2,3}-\d{5,9}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
     },
-    number: {
-        type: String,
-        validate: {
-            validator: function(v){
-                return /\d{2,3}-\d{5,9}/.test(v);
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        },
-        required: true
-    }});
+    required: true
+  }});
 
-personSchema.set('toJSON', {
-    transform: (document, returndedObject) => {
-        returndedObject.id = returndedObject._id.toString();
-        delete returndedObject._id;
-        delete returndedObject.__v;
-    }
+personSchema.set("toJSON", {
+  transform: (document, returndedObject) => {
+    returndedObject.id = returndedObject._id.toString();
+    delete returndedObject._id;
+    delete returndedObject.__v;
+  }
 });
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model("Person", personSchema);
